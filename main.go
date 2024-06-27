4ce08ee07f7f21635949cef9456446e9924bbea4
Barry Hanna <barryahanna@gmail.com>
2024-06-27 21:01:48 +0100

    help and exit commands

--- main.go
+++ main.go
@@ -1,11 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandExit() error {
+	fmt.Println("Exiting...")
+	defer os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println(`Welcome to the Pokedex!
+Usage:
+
+help: Displays a help message
+exit: Exit the Pokedex`)
+	return nil
+}
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
 	for {
-		var input string
 		fmt.Print("pokedex >")
-		fmt.Scan(&input)
+		for scanner.Scan() {
+			command := scanner.Text()
+			commands[command].callback()
+			fmt.Print("pokedex >")
+		}
 	}
 }
